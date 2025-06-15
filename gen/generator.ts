c616a7821546a882b76205e7919c7d36f7b66b9e
kristiker <kristik06@outlook.com>
2025-06-15 19:05:22 +0200

    Heat treated calculation

--- gen/generator.ts
+++ gen/generator.ts
@@ -1,41 +1,126 @@
+// @ts-nocheck
+
 import fs from "fs";
 import sharp from "sharp";
+import srgbtransform from "./utils/srgb-transform";
 
 class BlueGemGenerator {
-  *getImageFiles(filter: string): Generator<string> {
-    for (const file of fs.readdirSync("images")) {
-      if (file.includes(filter)) {
-        yield `./images/${file}`;
-      }
-    }
+
+  // input: r,g,b in [0,1], out: h in [0,360) and s,v in [0,1]
+  static rgb2hsv(r: number, g: number, b: number): [number, number, number] {
+    let v=Math.max(r,g,b), c=v-Math.min(r,g,b);
+    let h= c && ((v==r) ? (g-b)/c : ((v==g) ? 2+(b-r)/c : 4+(r-g)/c));
+    return [60*(h<0?h+6:h), v&&c/v, v];
+  }
+
+  static IsHeatTreatedBlueHsv(hue: number, saturation: number, brightness: number): boolean {
+      if (hue < 0.55 || hue >= 0.7) return false;
+      if (saturation < 0.3 || saturation >= 1.0) return false;
+      if (brightness < 0.1 || brightness >= 1.0) return false;
+
+      return true;
+  }
+
+  static IsHeatTreatedPurpleHsv(hue: number, saturation: number, brightness: number): boolean {
+    if ((hue < 0.7 || hue >= 0.9)) return false;
+    if (saturation < 0.2 || saturation >= 1.0) return false;
+    if (brightness < 0.05 || brightness >= 1.0) return false;
+
+    return true;
   }
 
   async analyzeImage(imagePath: string): Promise<void> {
-    let blueCount = 0;
     const startTime = Date.now();
 
     const { info, data } = await sharp(fs.realpathSync(imagePath))
       .raw()
       .toBuffer({ resolveWithObject: true });
 
-    for (let i = 0; i < data.length; i += 4) {
-      const r = data[i];
-      const g = data[i + 1];
-      const b = data[i + 2];
-      const a = data[i + 3];
+    let blueCount = 0;
+    let totalCount = 0;
 
+    const WarnForDoubleClassification = false
+    const UseLinearColors = true;
+
+    for (let i = 0; i < data.length; i += 4) {
+      const a = data[i + 3];
       if (a == 0) {
         continue;
       }
 
-      if (b > 100) {
+      totalCount++;
+      let [r, g, b] = [data[i], data[i + 1], data[i + 2]];
+
+      if (UseLinearColors) {
+        [r, g, b] = [
+          srgbtransform.srgb8BitToLinear(r),
+          srgbtransform.srgb8BitToLinear(g),
+          srgbtransform.srgb8BitToLinear(b),
+        ];
+      } else {
+        r /= 255;
+        g /= 255;
+        b /= 255;
+      }
+
+      let [h, s, v] = BlueGemGenerator.rgb2hsv(r, g, b);
+      h /= 360; // Normalize hue to [0, 1]
+
+      const isBlue = BlueGemGenerator.IsHeatTreatedBlueHsv(h, s, v);
+      const isPurple = BlueGemGenerator.IsHeatTreatedPurpleHsv(h, s, v);
+
+      const isGold = !isBlue && !isPurple; // todo: gold classification
+
+      if (WarnForDoubleClassification) {
+        if (isBlue && isPurple) {
+          console.warn(`Double classification detected at pixel ${i / 4}: Blue and Purple`);
+        }
+      }
+
+      if (isBlue) {
         blueCount++;
+        data[i] = 0;
+        data[i + 1] = 0;
+        data[i + 2] = 255;
+      } else if (isPurple) {
+        data[i] = 255;
+        data[i + 1] = 0;
+        data[i + 2] = 255;
+      } else if (isGold) {
+        data[i] = 255;
+        data[i + 1] = 215;
+        data[i + 2] = 0;
+      } else {
+        data[i] = 0;
+        data[i + 1] = 0;
+        data[i + 2] = 0;
       }
     }
 
-    console.log(`Image: ${imagePath}`);
-    console.log(`Blue pixels: ${blueCount}`);
-    console.log(`Finished in ${(Date.now() - startTime) / 1000} seconds`);
+    // get percentage, rounded to 2 decimal places
+    const bluePercentage = Math.round(((blueCount / totalCount) * 100) * 100) / 100;
+    const finishedSeconds = (Date.now() - startTime) / 1000;
+
+    console.log(`Image: ${imagePath}, Blue percentage: ${bluePercentage}%, Finished in ${finishedSeconds}s`);
+
+    // write masked image for visualization
+    await sharp(
+      Buffer.from(data),
+      {
+        raw: {
+          width: info.width,
+          height: info.height,
+          channels: info.channels,
+        },
+      }).toFile("gen/last_image_masked.png");
+  }
+
+  *getImageFiles(filter: string): Generator<string> {
+    for (const file of fs.readdirSync("images")) {
+      if (file.includes(filter)) {
+        yield `./images/${file}`;
+      }
+    }
   }
 
   run(): void {
