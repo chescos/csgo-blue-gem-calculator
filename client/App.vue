ede9307970370459c51a08deec5541c37bf36ccf
chescos <ch3sc0s@gmail.com>
2025-06-18 15:21:10 +0200

    ui improvement

--- client/App.vue
+++ client/App.vue
@@ -144,167 +144,177 @@ export default {
 </script>
 
 <template>
-  <div class="p-4">
-    <!-- Top Menu -->
-    <div class="flex flex-wrap items-center justify-center">
-      <!-- Item Select -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="item">Item</label>
-        <div class="relative mt-2">
-          <select
-            id="item"
-            class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            v-model="activeItem"
-          >
-            <option v-for="(value, key) in data" :value="key">
-              {{ key }}
-            </option>
-          </select>
-          <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
+  <!-- Top Menu -->
+  <div class="flex flex-wrap items-center justify-center p-4">
+    <!-- Item Select -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="item">Item</label>
+      <div class="relative mt-2">
+        <select
+          id="item"
+          class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          v-model="activeItem"
+        >
+          <option v-for="(value, key) in data" :value="key">
+            {{ key }}
+          </option>
+        </select>
+        <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
             <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
               <path
                 d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
               ></path>
             </svg>
           </span>
-        </div>
-      </div>
-
-      <!-- Finish Select -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="finish">Finish</label>
-        <div class="relative mt-2">
-          <select
-            id="finish"
-            class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            v-model="activeFinish"
-          >
-            <option v-for="(value, key) in data[activeItem]" :value="key">
-              {{ key }}
-            </option>
-          </select>
-          <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
-            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
-              <path
-                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
-              ></path>
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      <!-- Pose Select -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="pose">Pose</label>
-        <div class="relative mt-2">
-          <select
-            id="pose"
-            class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            v-model="activePose"
-          >
-            <option v-for="(value, key) in data[activeItem][activeFinish]" :value="key">
-              {{ titleCase(key) }}
-            </option>
-          </select>
-          <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
-            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
-              <path
-                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
-              ></path>
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      <!-- Order Select -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="order">Order</label>
-        <div class="relative mt-2">
-          <select
-            id="order"
-            class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            v-model="activeOrder"
-          >
-            <option v-for="(value, key) in { blue: 'Blue', purple: 'Purple', gold: 'Gold', other: 'Other' }" :value="key">
-              {{ value }}
-            </option>
-          </select>
-          <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
-            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
-              <path
-                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
-              ></path>
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      <!-- Sort Select -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="sort">Sort</label>
-        <div class="relative mt-2">
-          <select
-            id="sort"
-            class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            v-model="activeSort"
-          >
-            <option v-for="(value, key) in { desc: 'Descending', asc: 'Ascending' }" :value="key">
-              {{ value }}
-            </option>
-          </select>
-          <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
-            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
-              <path
-                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
-              ></path>
-            </svg>
-          </span>
-        </div>
-      </div>
-
-      <!-- Seed Input -->
-      <div class="mx-4">
-        <label class="uppercase font-bold text-xs tracking-widest" for="seed">Seed</label>
-        <div class="mt-2">
-          <input
-            id="seed"
-            type="number"
-            min="0"
-            max="1000"
-            step="1"
-            class="w-24 appearance-none rounded p-3 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
-            placeholder="Seed"
-            aria-label="Seed"
-          />
-        </div>
       </div>
     </div>
 
-    <!-- Image -->
-    <div class="my-20">
-      <img class="block w-auto mx-auto" style="max-height: 50vh" :src="screenshotUrl" alt="Screenshot" />
+    <!-- Finish Select -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="finish">Finish</label>
+      <div class="relative mt-2">
+        <select
+          id="finish"
+          class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          v-model="activeFinish"
+        >
+          <option v-for="(value, key) in data[activeItem]" :value="key">
+            {{ key }}
+          </option>
+        </select>
+        <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
+            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
+              <path
+                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
+              ></path>
+            </svg>
+          </span>
+      </div>
     </div>
 
-    <!-- Table -->
-    <table class="rounded table-fixed overflow-hidden w-full">
-      <thead class="bg-gray-700 text-white text-left text-sm font-medium">
-        <tr>
-          <template v-for="(value, key) in data[activeItem][activeFinish]" :key="key">
-            <th v-for="value in ['blue', 'gold', 'purple', 'other']" class="px-4 py-3 uppercase">
-              {{ key }} {{ value }}
-            </th>
-          </template>
-        </tr>
-      </thead>
-      <tbody class="text-gray-400 divide-y divide-gray-700">
-        <tr class="">
-          <template v-for="(value, key) in data[activeItem][activeFinish]" :key="key">
-            <td v-for="value in ['blue', 'gold', 'purple', 'other']" class="p-4">
-              {{ data[activeItem][activeFinish][key][activeIndex][value].toFixed(2) }}%
-            </td>
-          </template>
-        </tr>
-      </tbody>
-    </table>
+    <!-- Pose Select -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="pose">Pose</label>
+      <div class="relative mt-2">
+        <select
+          id="pose"
+          class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          v-model="activePose"
+        >
+          <option v-for="(value, key) in data[activeItem][activeFinish]" :value="key">
+            {{ titleCase(key) }}
+          </option>
+        </select>
+        <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
+            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
+              <path
+                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
+              ></path>
+            </svg>
+          </span>
+      </div>
+    </div>
+
+    <!-- Order Select -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="order">Order</label>
+      <div class="relative mt-2">
+        <select
+          id="order"
+          class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          v-model="activeOrder"
+        >
+          <option v-for="(value, key) in { blue: 'Blue', purple: 'Purple', gold: 'Gold', other: 'Other' }" :value="key">
+            {{ value }}
+          </option>
+        </select>
+        <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
+            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
+              <path
+                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
+              ></path>
+            </svg>
+          </span>
+      </div>
+    </div>
+
+    <!-- Sort Select -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="sort">Sort</label>
+      <div class="relative mt-2">
+        <select
+          id="sort"
+          class="appearance-none rounded p-3 pr-10 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          v-model="activeSort"
+        >
+          <option v-for="(value, key) in { desc: 'Descending', asc: 'Ascending' }" :value="key">
+            {{ value }}
+          </option>
+        </select>
+        <span class="absolute text-gray-200 inset-y-0 right-0 flex items-center pr-3">
+            <svg class="w-3 h-3" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 12 7">
+              <path
+                d="M11.261 2.02A.96.96 0 009.941.623L6 4.35 2.06.623A.96.96 0 00.74 2.02l4.573 4.33a1 1 0 001.374 0l4.574-4.33z"
+              ></path>
+            </svg>
+          </span>
+      </div>
+    </div>
+
+    <!-- Seed Input -->
+    <div class="mx-4">
+      <label class="uppercase font-bold text-xs tracking-widest" for="seed">Seed</label>
+      <div class="mt-2">
+        <input
+          id="seed"
+          type="number"
+          min="0"
+          max="1000"
+          step="1"
+          class="w-24 appearance-none rounded p-3 bg-gray-700 text-white leading-tight focus:outline-none focus:shadow-outline"
+          placeholder="Seed"
+          aria-label="Seed"
+        />
+      </div>
+    </div>
   </div>
+
+  <!-- Image -->
+  <div class="flex">
+    <button class="text-gray-400 hover:text-white cursor-pointer p-4" type="button">
+      <svg class="w-10 h-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path d="M9.4 233.4c-12.5 12.5-12.5 32.8 0 45.3l192 192c12.5 12.5 32.8 12.5 45.3 0s12.5-32.8 0-45.3L77.3 256 246.6 86.6c12.5-12.5 12.5-32.8 0-45.3s-32.8-12.5-45.3 0l-192 192z"/>
+      </svg>
+    </button>
+
+    <img class="block w-auto mx-auto max-h-[50vh] my-20" :src="screenshotUrl" alt="Screenshot" />
+
+    <button class="text-gray-400 hover:text-white cursor-pointer p-4" type="button">
+      <svg class="w-10 h-auto" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 320 512">
+        <path d="M310.6 233.4c12.5 12.5 12.5 32.8 0 45.3l-192 192c-12.5 12.5-32.8 12.5-45.3 0s-12.5-32.8 0-45.3L242.7 256 73.4 86.6c-12.5-12.5-12.5-32.8 0-45.3s32.8-12.5 45.3 0l192 192z"/>
+      </svg>
+    </button>
+  </div>
+
+  <!-- Table -->
+  <table class="table-fixed overflow-hidden w-full">
+    <thead class="bg-gray-700 text-white text-left text-sm font-medium">
+    <tr>
+      <template v-for="(value, key) in data[activeItem][activeFinish]" :key="key">
+        <th v-for="value in ['blue', 'gold', 'purple', 'other']" class="p-4 uppercase">
+          {{ key }} {{ value }}
+        </th>
+      </template>
+    </tr>
+    </thead>
+    <tbody class="text-gray-400 divide-y divide-gray-700">
+    <tr class="">
+      <template v-for="(value, key) in data[activeItem][activeFinish]" :key="key">
+        <td v-for="value in ['blue', 'gold', 'purple', 'other']" class="p-4">
+          {{ data[activeItem][activeFinish][key][activeIndex][value].toFixed(2) }}%
+        </td>
+      </template>
+    </tr>
+    </tbody>
+  </table>
 </template>
