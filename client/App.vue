a37318a80e1e755e62c4e808ebe21dda5a718059
kristiker <kristik06@outlook.com>
2025-06-21 12:39:05 +0200

    Fix path for ak input image

--- client/App.vue
+++ client/App.vue
@@ -150,7 +150,8 @@ export default {
 
       if (this.activeImages) {
         baseUrl = 'https://cdn.csgoskins.gg/public/images/gems/v2/poses/';
-        fileName = `${itemKey}_${finishKey}_${this.activeRegion}_${selectedSeed.seed}.avif`;
+        const inputImageKey = ['playside', 'backside'].includes(this.activeRegion) ? this.activeRegion : 'playside';
+        fileName = `${itemKey}_${finishKey}_${inputImageKey}_${selectedSeed.seed}.avif`;
       } else {
         baseUrl = 'https://cdn.csgoskins.gg/public/images/gems/v2/screenshots/';
 
@@ -379,7 +380,7 @@ export default {
       </div>
     </div>
 
-    <!-- Pose Images Toggle -->
+    <!-- Albedo Toggle -->
     <div class="mx-4">
       <label class="uppercase font-bold text-xs tracking-widest flex" for="seed">
         <svg class="w-4 h-4 mr-2" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 576 512">
@@ -387,12 +388,13 @@ export default {
             d="M288 32c-80.8 0-145.5 36.8-192.6 80.6C48.6 156 17.3 208 2.5 243.7c-3.3 7.9-3.3 16.7 0 24.6C17.3 304 48.6 356 95.4 399.4C142.5 443.2 207.2 480 288 480s145.5-36.8 192.6-80.6c46.8-43.5 78.1-95.4 93-131.1c3.3-7.9 3.3-16.7 0-24.6c-14.9-35.7-46.2-87.7-93-131.1C433.5 68.8 368.8 32 288 32zM144 256a144 144 0 1 1 288 0 144 144 0 1 1 -288 0zm144-64c0 35.3-28.7 64-64 64c-7.1 0-13.9-1.2-20.3-3.3c-5.5-1.8-11.9 1.6-11.7 7.4c.3 6.9 1.3 13.8 3.2 20.7c13.7 51.2 66.4 81.6 117.6 67.9s81.6-66.4 67.9-117.6c-11.1-41.5-47.8-69.4-88.6-71.1c-5.8-.2-9.2 6.1-7.4 11.7c2.1 6.4 3.3 13.2 3.3 20.3z"
           />
         </svg>
-        <span>Pose Images</span>
+        <span>Albedo</span>
       </label>
       <label class="block items-center cursor-pointer mt-3">
         <input type="checkbox" v-model="activeImages" class="sr-only peer" />
         <span
           class="block relative w-20 h-11 bg-gray-700 peer-focus:outline-none rounded peer peer-checked:after:translate-x-full peer-checked:after:border-white after:content-[''] after:absolute after:top-[4px] after:start-[4px] after:bg-white after:border-gray-300 after:border after:rounded after:h-9 after:w-9 after:transition-all peer-checked:bg-blue-600 transition-colors"
+          title="Display clean image used for percentage calculation"
         ></span>
       </label>
     </div>
